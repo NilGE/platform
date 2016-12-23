7ab54112e8cc60a869132eeb339468e9ee4c6c08
郑惠文 <huiwenzh@usc.edu>
2016-12-23 15:56:43 -0800

    book related console.log deleted

--- src/reducers/bookReducers.js
+++ src/reducers/bookReducers.js
@@ -1,16 +1,13 @@
 import * as actionTypes from '../actions/actionTypes';
 
 export const booksReducer = (state = [], action) => {
-  console.log('action.type: ' + action.type);
   switch (action.type) {
   case actionTypes.CREATE_BOOK_SUCCESS:
-    console.log(action.type);
     return [
       ...state,
       Object.assign({}, action.book)
     ];
   case actionTypes.FETCH_BOOKS_SUCCESS:
-    console.log(action.books);
     return action.books;
   default:
     return state;
