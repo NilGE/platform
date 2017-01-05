e394e3197915f63cd8b141c15d5a3d5232578c6f
NilGE <gxy26904@gmail.com>
2017-01-04 23:29:23 -0800

    remove product reducer and action

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { usersReducer, authReducer } from './userReducers';
-import { productReducer } from './productReducers';
 import { flashMessages } from './flashMessages';
 import { houseReducer, housesReducer } from './houseReducers';
 
@@ -8,7 +7,6 @@ export default combineReducers({
   users: usersReducer,
   flashMessages: flashMessages,
   auth: authReducer,
-  product: productReducer,
   house: houseReducer,
   houses: housesReducer
 });
